8c85f495a8a482f029f2dca00a2d0042511c5115
olalaurao <lauribeirolopes@gmail.com>
2026-09-23 01:10:49 -0300

    chore: bump Gate 5 image spike build

--- readwisereader.koplugin/constants.lua
+++ readwisereader.koplugin/constants.lua
@@ -1,7 +1,7 @@
 -- SPDX-License-Identifier: AGPL-3.0-only
 
 return {
-    VERSION = "0.1.16",
+    VERSION = "0.1.17",
     SETTINGS_FILENAME = "readwisereader.lua",
     DEFAULT_DOWNLOAD_ROOT = "/mnt/us/documents/Readwise",
     DEFAULT_SYNC_LOCATIONS = { "new", "later" },
