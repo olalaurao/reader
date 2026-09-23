d1c6c4b022f508d0379b4a28e71769f9d8d94a6e
olalaurao <lauribeirolopes@gmail.com>
2026-09-23 11:06:02 -0300

    test: run raw source coverage

--- readwisereader.koplugin/tests/run.lua
+++ readwisereader.koplugin/tests/run.lua
@@ -9,6 +9,7 @@ local tests = {
     "tests.test_http",
     "tests.test_reader",
     "tests.test_reader_pagination",
+    "tests.test_raw_source",
     "tests.test_metadata_scan",
     "tests.test_library_ui",
     "tests.test_article_ui",
