73ccecc44124e1d774cee0a5c5415973196697ba
olalaurao <lauribeirolopes@gmail.com>
2026-09-23 12:18:25 -0300

    test: run Readwise v2 interop coverage

--- readwisereader.koplugin/tests/run.lua
+++ readwisereader.koplugin/tests/run.lua
@@ -8,6 +8,7 @@ local tests = {
     "tests.test_config",
     "tests.test_http",
     "tests.test_reader",
+    "tests.test_readwise",
     "tests.test_reader_pagination",
     "tests.test_raw_source",
     "tests.test_raw_format_ui",
