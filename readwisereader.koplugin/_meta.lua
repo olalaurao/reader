0320a541c56c49d8f2a035e9c1b157aba41913dc
olalaurao <lauribeirolopes@gmail.com>
2026-09-22 23:53:56 -0300

    docs: identify 0.1.12 Bookshelf integration build

--- readwisereader.koplugin/_meta.lua
+++ readwisereader.koplugin/_meta.lua
@@ -5,5 +5,5 @@ local _ = require("gettext")
 return {
     name = "readwisereader",
     fullname = _("Readwise Reader"),
-    description = _([[Synchronize Readwise Reader content with KOReader. This experimental 0.1.3 build adds the Gate 4 document-sync flow.]]),
+    description = _([[Synchronize Readwise Reader content with KOReader. Experimental 0.1.12 adds Gate 4A Bookshelf tag metadata projection.]]),
 }
