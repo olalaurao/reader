bb43dc0f22aa73193dfb77e5dc65166d53c3a6d0
olalaurao <lauribeirolopes@gmail.com>
2026-09-23 10:41:19 -0300

    docs: identify 0.1.19 responsive-image build

--- readwisereader.koplugin/_meta.lua
+++ readwisereader.koplugin/_meta.lua
@@ -5,5 +5,5 @@ local _ = require("gettext")
 return {
     name = "readwisereader",
     fullname = _("Readwise Reader"),
-    description = _([[Synchronize Readwise Reader content with KOReader. Experimental 0.1.18 adds bounded offline article images for Gate 5.]]),
+    description = _([[Synchronize Readwise Reader content with KOReader. Experimental 0.1.19 fixes Reader responsive/lazy image discovery for Gate 5.]]),
 }
