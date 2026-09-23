f6c6a4839f5dc2eab456fdbebbaa0fac9d2b36c2
olalaurao <lauribeirolopes@gmail.com>
2026-09-23 10:20:01 -0300

    docs: identify 0.1.18 bounded image build

--- readwisereader.koplugin/_meta.lua
+++ readwisereader.koplugin/_meta.lua
@@ -5,5 +5,5 @@ local _ = require("gettext")
 return {
     name = "readwisereader",
     fullname = _("Readwise Reader"),
-    description = _([[Synchronize Readwise Reader content with KOReader. Experimental 0.1.17 adds the Phase G relative-image asset spike.]]),
+    description = _([[Synchronize Readwise Reader content with KOReader. Experimental 0.1.18 adds bounded offline article images for Gate 5.]]),
 }
