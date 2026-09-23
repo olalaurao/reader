3fcf74df64c79f97a1199afd0de579e0bb6b46f8
olalaurao <lauribeirolopes@gmail.com>
2026-09-22 21:16:18 -0300

    test(io): cover installer failure stage classification

--- readwisereader.koplugin/tests/test_installer.lua
+++ readwisereader.koplugin/tests/test_installer.lua
@@ -62,6 +62,19 @@ local function fakeDeps(existing)
             end,
         },
     }
+    do
+        local fake = fakeDeps()
+        fake.deps.open_file = function()
+            return nil, "simulated open failure"
+        end
+        local installer = Installer:new{ deps = fake.deps }
+        local result, err = installer:install("new", "/root/Articles/doc.html")
+        assert(result == nil)
+        assert(err.kind == "io")
+        assert(err.retryable == true)
+        assert(err.stage == "open")
+    end
+
 end
 
 return function()
