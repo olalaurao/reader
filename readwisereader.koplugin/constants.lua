828e8475975d45ac977ee2a87e00d821e141c1b1
olalaurao <lauribeirolopes@gmail.com>
2026-09-24 10:17:09 -0300

    chore: bump Phase O backlog build to 0.1.37

--- readwisereader.koplugin/constants.lua
+++ readwisereader.koplugin/constants.lua
@@ -1,7 +1,7 @@
 -- SPDX-License-Identifier: AGPL-3.0-only
 
 return {
-    VERSION = "0.1.36",
+    VERSION = "0.1.37",
     SETTINGS_FILENAME = "readwisereader.lua",
     DEFAULT_DOWNLOAD_ROOT = "/mnt/us/documents/Readwise",
     DEFAULT_SYNC_LOCATIONS = { "new", "later" },
