230f73084aebba9dabba55068ffe853dad8b8102
olalaurao <lauribeirolopes@gmail.com>
2026-09-25 00:56:28 -0300

    chore: version Gate 17B alpha

--- readwisereader.koplugin/constants.lua
+++ readwisereader.koplugin/constants.lua
@@ -1,7 +1,7 @@
 -- SPDX-License-Identifier: AGPL-3.0-only
 
 return {
-    VERSION = "1.1.0-alpha.1",
+    VERSION = "1.1.0-alpha.2",
     SETTINGS_FILENAME = "readwisereader.lua",
     DEFAULT_DOWNLOAD_ROOT = "/mnt/us/documents/Readwise",
     DEFAULT_SYNC_LOCATIONS = { "new", "later" },
