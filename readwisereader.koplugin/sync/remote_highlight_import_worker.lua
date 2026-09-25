109c1fbe95e854bf645837c2480b6ca8f143c1c4
olalaurao <lauribeirolopes@gmail.com>
2026-09-25 10:24:00 -0300

    feat: wire Readwise deletion feed into highlight cache

--- readwisereader.koplugin/sync/remote_highlight_import_worker.lua
+++ readwisereader.koplugin/sync/remote_highlight_import_worker.lua
@@ -14,6 +14,7 @@ function Worker:run(local_path)
     local SyncMeta = require("storage/sync_meta")
     local Http = require("api/http")
     local Reader = require("api/reader")
+    local Readwise = require("api/readwise")
     local Cache = require("sync/remote_highlight_cache")
 
     local config = Config:new()
@@ -42,9 +43,14 @@ function Worker:run(local_path)
             )
         end
 
+        local http = Http:new()
         local cache = Cache:new{
             reader = Reader:new{
-                http = Http:new(),
+                http = http,
+                config = config,
+            },
+            readwise = Readwise:new{
+                http = http,
                 config = config,
             },
             repository = RemoteHighlights:new{ db = db },
@@ -67,6 +73,9 @@ function Worker:run(local_path)
             cache_mode = refresh.mode,
             cache_rows_seen = refresh.rows_seen,
             cache_rows_upserted = refresh.rows_upserted,
+            cache_deleted_highlights = refresh.deleted_highlight_ids or 0,
+            cache_deleted_parents = refresh.deleted_parent_ids or 0,
+            cache_deletion_pages = refresh.deletion_pages or 0,
             cache_scan_started_at = refresh.scan_started_at,
             cache_query_after = refresh.proposed_query_after,
             updated_after = refresh.updated_after,
